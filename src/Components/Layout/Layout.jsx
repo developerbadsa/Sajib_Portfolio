1c7e7050b374f77a199910dd95f4d8770250b7f2
Rahim Badsa <developerbadsa@gmail.com>
2024-03-09 14:04:53 +0600

    added footer

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import { StickyNavbar } from '../Shared/Header/Header';
+import Footer from '../Shared/Footer/Footer';
 
 const Layout = () => {
  return (
   <div className='max-w-[1280px] mx-auto border'>
     <StickyNavbar></StickyNavbar>
     <Outlet></Outlet>
-    footer
+    <Footer></Footer>
   </div>
  );
 };
